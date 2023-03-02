e29a941a59f4422da226d82984dfea53bd3f424c
Hirwa Aldo <51756396+hirwaaldo1@users.noreply.github.com>
2023-03-02 19:45:54 +0200

    fix: remove react component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
